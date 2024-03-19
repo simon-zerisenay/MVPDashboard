8ea2537d50a59b60c1b8df4f7da111ece0790b2d
Simon Z. Ghebremeskel <81813787+simon-zerisenay@users.noreply.github.com>
2024-03-19 22:21:36 +0400

    Update connection.py

--- connection.py
+++ connection.py
@@ -4,7 +4,7 @@ import os
 connection_string = "DefaultEndpointsProtocol=https;AccountName=pistoragefrc1;AccountKey=0JfcY7Sg9tyRMLeaupyQBO3S3UUx8rAJl6jx2WAylYesrQPITUUzDRxKgtdI4b3Qtj68Hgf51oT/+AStb2zSPA==;EndpointSuffix=core.windows.net"
 
 
-
+ 
 container_name = "containerfrc" 
 
 try:
